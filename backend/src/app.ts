7cfc974da03e885c0eee4870b288ef58d23263a7
HSrinivasBharadwaj <srinivasbharadwaj.hullur@gmail.com>
2026-02-01 16:55:16 +0530

    Added the userProfile Router

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,12 +2,14 @@ import express from "express";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/authRoutes";
+import userProfileRouter from './routes/userProfileRoutes';
 const app = express();
 
 app.use(express.json())
 app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
+app.use("/api/users", userProfileRouter);
 
 
 app.listen(process.env.PORT,() => {
